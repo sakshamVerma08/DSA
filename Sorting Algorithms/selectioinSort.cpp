13ccb932946d9af76f8caddaef7ca71e8b09571b
Saksham Verma <sakshamverma1000@gmail.com>
2024-10-13 10:00:02 +0530

    Completed the Selection sort code

--- Sorting Algorithms/selectioinSort.cpp
+++ Sorting Algorithms/selectioinSort.cpp
@@ -5,7 +5,24 @@ using namespace std;
 class Solution
 {
 public:
-    void selectionSort(vector<int> &arr) {}
+    void selectionSort(vector<int> &arr)
+    {
+        if (arr.size() == 0 || arr.size() == 1)
+            return;
+
+        for (int i = 0; i < arr.size() - 1; i++)
+        {
+            int minIndex = i;
+
+            for (int j = i + 1; j < arr.size(); j++)
+            {
+                if (arr[j] < arr[minIndex])
+                    minIndex = j;
+            }
+
+            swap(arr[i], arr[minIndex]);
+        }
+    }
 
     void printVector(vector<int> &arr)
     {
@@ -21,6 +38,10 @@ public:
 
 int main()
 {
-
+    Solution sol;
     vector<int> v1 = {40, 20, 60, 50, 10};
+    sol.selectionSort(v1);
+    sol.printVector(v1);
+
+    return 0;
 }
