32008d3e81c938a754c4eecc445e0608973f5b87
Saksham Verma <sakshamverma1000@gmail.com>
2024-10-19 07:00:25 +0530

    Did the problem to print all subsequences of the array

--- Recursion/problems_on_FunctionRecursion.cpp
+++ Recursion/problems_on_FunctionRecursion.cpp
@@ -92,11 +92,50 @@ public:
 
     // The time complexity of the above function will be roughly
     // equal to 2^n.
+
+    void printVector(vector<int> &v)
+    {
+        if (v.size() == 0)
+            cout << "[]";
+
+        else
+        {
+            for (auto elem : v)
+            {
+                cout << elem << " ";
+            }
+            cout << "\n";
+        }
+    }
+
+    void printArraySubsequences(int arr[], vector<int> &ans, int n, int index)
+    {
+        if (index >= n)
+        {
+            printVector(ans);
+            return;
+        }
+
+        ans.push_back(arr[index]);
+
+        // Take the current index element in your subsequence
+        printArraySubsequences(arr, ans, n, index + 1);
+
+        // Don't take the current index element in your subsequence
+        ans.pop_back();
+        printArraySubsequences(arr, ans, n, index + 1);
+    }
+    // Time complexity of above code = O(2^n) x n
+    // Space complexity of above code = O(N)
+    
 };
 
 int main()
 {
     Solution sol;
-    
+    int arr[] = {1, 2, 3, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    vector<int> ans;
+    sol.printArraySubsequences(arr, ans, n, 0);
     return 0;
 }
