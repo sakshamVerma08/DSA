435224842e10d8374fdfc8b31579852a21b45075
Saksham Verma <sakshamverma1000@gmail.com>
2024-10-19 07:42:53 +0530

    Did printArraySubsequences whose sum = k

--- Recursion/subsequenceProblems.cpp
+++ Recursion/subsequenceProblems.cpp
@@ -38,14 +38,35 @@ public:
     }
     // Time complexity of above code = O(2^n) x n
     // Space complexity of above code = O(N)
+
+    void kSumSubsequences(int arr[], vector<int> &ans, int n, int index, int sum, int s)
+    { // Prints those subsequences only whose sum is equal to the given value for the sum.
+        if (index == n)
+        {
+            if (s == sum)
+                printVector(ans);
+
+            return;
+        }
+
+        ans.push_back(arr[index]);
+        s += arr[index];
+        // Pick the current element in the subsequence.
+        kSumSubsequences(arr, ans, n, index + 1, sum, s);
+        ans.pop_back();
+        s -= arr[index];
+        // Don't pick the current element in the subsequence.
+        kSumSubsequences(arr, ans, n, index + 1, sum, s);
+    }
 };
 
 int main()
 {
     Solution sol;
-    int arr[] = {1, 2, 3, 4};
+    int arr[] = {1, 2, 3};
     int n = sizeof(arr) / sizeof(arr[0]);
+    int sum = 3;
     vector<int> ans;
-    sol.printArraySubsequences(arr, ans, n, 0);
+    sol.kSumSubsequences(arr, ans, n, 0, sum, 0);
     return 0;
 }
