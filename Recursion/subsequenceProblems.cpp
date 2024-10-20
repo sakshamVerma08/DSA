77915f8581df11d61dd6591d23b5b1510d2d2c1a
Saksham Verma <sakshamverma1000@gmail.com>
2024-10-20 07:13:59 +0530

    Did all the important subsequence patterns

--- Recursion/subsequenceProblems.cpp
+++ Recursion/subsequenceProblems.cpp
@@ -39,7 +39,7 @@ public:
     // Time complexity of above code = O(2^n) x n
     // Space complexity of above code = O(N)
 
-    void kSumSubsequences(int arr[], vector<int> &ans, int n, int index, int sum, int s)
+    void printKSubsequences(int arr[], vector<int> &ans, int n, int index, int sum, int s)
     { // Prints those subsequences only whose sum is equal to the given value for the sum.
         if (index == n)
         {
@@ -52,21 +52,79 @@ public:
         ans.push_back(arr[index]);
         s += arr[index];
         // Pick the current element in the subsequence.
-        kSumSubsequences(arr, ans, n, index + 1, sum, s);
+        printKSubsequences(arr, ans, n, index + 1, sum, s);
         ans.pop_back();
         s -= arr[index];
         // Don't pick the current element in the subsequence.
-        kSumSubsequences(arr, ans, n, index + 1, sum, s);
+        printKSubsequences(arr, ans, n, index + 1, sum, s);
+    }
+
+    bool printOneSubsequence(int arr[], vector<int> &ans, int n, int index, int sum, int s)
+    {
+        // Prints only one random subsequence whose sum is equal to k
+
+        if (index == n)
+        {
+
+            if (s == sum)
+            {
+                printVector(ans);
+                return 1;
+            }
+
+            return 0;
+        }
+
+        s += arr[index];
+        ans.push_back(arr[index]);
+        // Pick the element at current index. We have to check whether after picking this element, have we got our "one subsequence" or not. If yes, then no need to run the recursion for "Not picking the current element".
+        if (printOneSubsequence(arr, ans, n, index + 1, sum, s) == 1)
+        {
+            return 1;
+        }
+
+        ans.pop_back();
+        s -= arr[index];
+
+        if (printOneSubsequence(arr, ans, n, index + 1, sum, s) == 1)
+            return 1;
+
+        else
+            return 0;
+    }
+
+    int CountSubsequences(int arr[], int n, int index, int sum, int s)
+    {
+        // This functions gives the number of subsequences whose sum is equal to k.
+        // Time complexity of the function is O(2^n) because we have n elements, and for each element,
+        // we are choosing two options.
+
+        if (index == n)
+        {
+            if (s == sum)
+                return 1;
+
+            return 0;
+        }
+
+        s += arr[index];
+        int left = CountSubsequences(arr, n, index + 1, sum, s);
+
+        s -= arr[index];
+        int right = CountSubsequences(arr, n, index + 1, sum, s);
+
+        return left + right;
     }
 };
 
 int main()
 {
     Solution sol;
-    int arr[] = {1, 2, 3};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int sum = 3;
+    int arr[] = {1, 2, 3, 4};
+    int N = sizeof(arr) / sizeof(arr[0]);
     vector<int> ans;
-    sol.kSumSubsequences(arr, ans, n, 0, sum, 0);
+    int sum = 3;
+    cout << "Count = " << sol.CountSubsequences(arr, N, 0, sum, 0) << endl;
+    sol.printKSubsequences(arr, ans, N, 0, sum, 0);
     return 0;
 }
