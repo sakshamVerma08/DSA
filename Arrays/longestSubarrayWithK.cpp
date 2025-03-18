b3d6177c92e971af77310d2eb16f5873dd179b89
Saksham Verma <sakshamverma1000@gmail.com>
2025-03-18 14:05:55 +0530

    did one array question

--- Arrays/longestSubarrayWithK.cpp
+++ Arrays/longestSubarrayWithK.cpp
@@ -2,48 +2,82 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int longestSubarrayWithSumK(vector<int> a, long long k) {
-    vector<int> v;
-    int sum,count;
+// Using brute force solution (select each and every subarray and check for it)
+// time complexity: O(N^3)
+// space complexity: O(1)
 
-    for(int i = 0; i < a.size(); i ++){
+// First loop takes all starting indexes of all subarrays
+// Second loop takes all ending indexes of all subarrays
+// The third loop is suppossed to loop inside a particular subarray of size arr[i,j].
 
-        sum=0;
-        count=0;
+int longestSubarrayWithSumK(vector<int> a, long long k)
+{
+    int length = 0;
+    int sum;
 
-        for(int j = i; j < a.size();j++){
+    for (int i = 0; i < a.size(); i++)
+    {
 
-            sum += a[j];
-            count ++;
+        for (int j = i; j < a.size(); j++)
+        {
 
-            if(sum==k){
-                v.push_back(count);
-                break;
+            sum = 0;
+            // for each subarray
+            for (int k = i; k <= j; k++)
+            {
+                sum += a[k];
             }
-            
-            if(sum > k)
-            break;
 
+            if (sum == k)
+                length = max(length, j - i + 1);
         }
     }
 
-    int maxCnt = 0;
-
-    for(auto &elem : v){
-        if(elem > maxCnt)
-        maxCnt = elem;
-    }
-
-    return maxCnt;
+    return length;
 }
 
+// Optimized Brute force solution
+// time complexity: O(N^2)
+// space complexity: O(1)
+int longestSubarrayWithSumK2(vector<int> a, long long k)
+{
+    int length = 0;
+    int sum;
 
-int main(){
+    for (int i = 0; i < a.size(); i++)
+    {
 
-    vector<int> tc1 = {8 ,15,17,0,11};
+        sum = 0;
+
+        for (int j = i; j < a.size(); j++)
+        {
+
+            sum += a[j];
+
+            if (sum > k)
+                break;
+
+            else if (sum == k)
+            {
+                length = max(length, j - i + 1);
+                continue;
+            }
+        }
+    }
+
+    return length;
+}
+
+int longestSubarrayWithSumK3(vector<int> a, long long k)
+{
+}
+int main()
+{
+
+    vector<int> tc1 = {8, 15, 17, 0, 11};
     int n1 = tc1.size();
 
-    cout<<"tc1 : "<< longestSubarrayWithSumK(tc1,n1)<<endl;
+    cout << "tc1 : " << longestSubarrayWithSumK(tc1, n1) << endl;
 
     return 0;
 }
--- Hashing/countMaxFrequency.cpp
+++ Hashing/countMaxFrequency.cpp
@@ -2,72 +2,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// brute force approach
-// time complexity: O(N^2)
-// space comlexity: O(N)
+// Uses sliding window and sorting approach
+// time complexity: O(N logN)
+// space complexity: O(1)
 int maxFrequency(vector<int> &nums, int k)
 {
-    map<int, int> mpp;
-    int diff;
-    int temp;
-    for (auto &elem : nums)
+
+    sort(nums.begin(), nums.end());
+    // O(logN)
+
+    long L = 0, R = 0, total = 0, res = 0;
+
+    while (R < nums.size())
     {
-        temp = k;
-        if (mpp.find(elem) != mpp.end())
+
+        total += nums[R];
+
+        // Window Shrinking Loop (Invalid Window)
+        while (nums[R] * (R - L + 1) > total + k)
         {
-            mpp[elem]++;
+            total -= nums[L];
+            L++;
             continue;
         }
 
-        for (auto &e : nums)
-        {
-
-            if (e == elem)
-            {
-                mpp[e]++;
-                continue;
-            }
-
-            if (e > elem)
-                break;
-
-            diff = elem - e;
-
-            if (diff <= temp)
-            {
-                mpp[elem]++;
-                temp -= diff;
-            }
-
-            else
-                continue;
-        }
+        // Window Expanding part (Valid Window)
+        res = max(res, R - L + 1);
+        R++;
     }
 
-    int maxCnt = 0;
-
-    for (auto &iter : mpp)
-    {
-        if (iter.second > maxCnt)
-            maxCnt = iter.second;
-    }
-
-    return maxCnt;
-}
-
-int main()
-{
-
-    vector<int> tc1 = {1, 1, 4, 8, 13};
-    int k1 = 5;
-
-    vector<int> tc2 = {9930, 9923, 9983, 9997, 9934, 9952, 9945, 9914, 9985, 9982, 9970, 9932, 9985, 9902, 9975, 9990, 9922, 9990, 9994, 9937, 9996, 9964, 9943, 9963, 9911, 9925, 9935, 9945, 9933, 9916, 9930, 9938, 10000, 9916, 9911, 9959, 9957, 9907, 9913, 9916, 9993, 9930, 9975, 9924, 9988, 9923, 9910, 9925, 9977, 9981, 9927, 9930, 9927, 9925, 9923, 9904, 9928, 9928, 9986, 9903, 9985, 9954, 9938, 9911, 9952, 9974, 9926, 9920, 9972, 9983, 9973, 9917, 9995, 9973, 9977, 9947, 9936, 9975, 9954, 9932, 9964, 9972, 9935, 9946, 9966};
-    int k2 = 3056;
-
-    vector<int> tc3 = {1, 4, 8, 13, 8};
-    int k3 = 5;
-
-    // cout << "tc1 : " << maxFrequency(tc1, k1) << endl;
-    cout << "tc2 : " << maxFrequency(tc2, k2) << endl;
-    return 0;
+    return res;
 }
