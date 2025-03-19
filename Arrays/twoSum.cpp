80b0124922551d581e7eb9f1e785129319d0f14d
Saksham Verma <sakshamverma1000@gmail.com>
2025-03-19 09:02:47 +0530

    completed TwoSum problem

--- Arrays/twoSum.cpp
+++ Arrays/twoSum.cpp
@@ -68,6 +68,37 @@ vector<int> twoSum2(vector<int> &nums, int target)
 // Space complexity: O(1)
 vector<int> twoSum3(vector<int> &nums, int target)
 {
+    vector<int> vec;
+    vector<pair<int, int>> original;
+    int N = nums.size();
+    int left = 0, right = N - 1;
+
+    for (int i = 0; i < N; i++)
+    {
+        original.push_back({nums[i], i});
+    }
+    nums.clear();
+    sort(original.begin(), original.end());
+
+    while (right > left)
+    {
+        int sum = original[left].first + original[right].first;
+
+        if (sum == target)
+        {
+            vec.push_back(original[left].second);
+            vec.push_back(original[right].second);
+            break;
+        }
+
+        else if (sum < target)
+            left++;
+
+        else
+            right--;
+    }
+
+    return vec;
 }
 
 void printVector(vector<int> &v)
@@ -84,7 +115,7 @@ int main()
     vector<int> tc1 = {3, 2, 4};
     int target1 = 6;
 
-    vector<int> ans = twoSum2(tc1, target1);
+    vector<int> ans = twoSum3(tc1, target1);
     printVector(ans);
 
     return 0;
