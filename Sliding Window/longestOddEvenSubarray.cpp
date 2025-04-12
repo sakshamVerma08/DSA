fd12745c85b490716b54ed1df31961eb91c2ff05
Saksham Verma <sakshamverma1000@gmail.com>
2025-04-12 23:59:45 +0530

    today's POTD done, and one sliding window problem done

--- Sliding Window/longestOddEvenSubarray.cpp
+++ Sliding Window/longestOddEvenSubarray.cpp
@@ -1,53 +1,38 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
+
+// time complexity : O(N)
+// space complexity : O(1)
 int longestAlternatingSubarray(vector<int> &nums, int threshold)
 {
-    int ans = INT_MIN;
-    int start = 0;
+    int ans = 0;
     int n = nums.size();
-    int lastElem;
-    for (int end = 0; end < n; end++)
-    {
+    int end = 0;
 
-        if (nums[end] % 2 != 0 && nums[end] > threshold)
-            end++;
+    while (end < n)
+    {
+        if (nums[end] % 2 == 0 && nums[end] <= threshold)
+        {
+            // now we start to slide the window. To simulate the window sliding inside the array. we don't use the end pointer, otherwise it will messup the ending of current window in our array, instead we assing value of end to a 'temp' variable.
+            int temp = end;
+            while (temp + 1 < n && nums[temp] % 2 != nums[temp + 1] % 2 && nums[temp + 1] <= threshold)
+            {
+                temp++;
+            }
+
+            ans = max(ans, temp - end + 1);
+            // we reassign start of the window to end + 1, because once our subarray is broken, there is no point to scane other elements of that subarray, so we start from end + 1.
+            end = temp + 1;
+        }
 
         else
         {
-            start = end;
-            lastElem = nums[end];
             end++;
-
-            while (end < n - 1)
-            {
-                if (lastElem % 2 != nums[end] % 2 && nums[end] <= threshold)
-                {
-                    lastElem = nums[end];
-                    end++;
-                }
-
-                else
-                {
-                    start = end;
-                    lastElem = nums[end];
-
-                    if (nums[end] > threshold)
-                    {
-                        start++;
-                        end++;
-                    }
-
-                    else
-                        end++;
-                }
-            }
-
-            ans = max(ans, end - start + 1);
         }
     }
 
-    return ans - 1;
+    return ans;
 }
 int main()
 {
