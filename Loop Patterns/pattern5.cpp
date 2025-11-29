58f9baac3d911859855fd02eb35c1291feb61f38
Saksham Verma <sakshamverma1000@gmail.com>
2025-11-29 09:45:37 +0530

    Did some more loop pattern problems

--- Loop Patterns/pattern5.cpp
+++ Loop Patterns/pattern5.cpp
@@ -3,4 +3,17 @@ using namespace std;
 
 int main()
 {
+
+    for (int i = 0; i < 5; i++)
+    {
+
+        for (int j = (4 - i); j >= 0; j--)
+        {
+
+            cout << '*';
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
---
+++ Loop Patterns/pattern7.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    for (int i = 0; i < 5; i++)
+    {
+
+        for (int j = 0; j < 5 - i; j++)
+        {
+            cout << " ";
+        }
+
+        for (int k = 0; k < 2 * i + 1; k++)
+        {
+            cout << '*';
+        }
+
+        for (int j = 0; j < 5 - i; j++)
+        {
+            cout << " ";
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
