c9b694f7080108e73692ea8dfc80eab9e3e46d31
Saksham Verma <sakshamverma1000@gmail.com>
2026-04-06 08:58:50 +0530

    Did cycle detection in an unidrected graph using DFS & BFS, also did cycle detection in directed graph using DFS

--- NeetCode_150/graph/cycleDetection.cpp
+++ NeetCode_150/graph/cycleDetection.cpp
@@ -2,57 +2,206 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> constructAdjList(int n, int m)
+class UndirectedGraph
 {
-    vector<vector<int>> adj_list(n + 1, vector<int>{});
+private:
+    vector<vector<int>> adjList;
+    vector<int> visited;
+    int n, m;
 
-    for (int i = 0; i < m; i++)
+public:
+    UndirectedGraph(int n, int m)
     {
-        // looping from 0 to m-1 (inclusive).
-        int u, v;
-        cin >> u >> v;
-        adj_list[u].emplace_back(v);
-        adj_list[v].emplace_back(u);
+        this->adjList = vector<vector<int>>(n + 1, vector<int>{});
+        this->visited = vector<int>(n + 1, 0);
+        this->n = n;
+        this->m = m;
     }
-    return adj_list;
-}
 
-bool detectCycle(vector<vector<int>> &adjList, vector<int> &visited, int start)
-{
-
-    queue<pair<int, int>> q;
-    q.push({start, -1});
-
-    while (!q.empty())
+    void constructAdjList(int n, int m)
     {
-        int node = q.front().first, parent = q.front().second;
-        q.pop();
-        visited[node] = 1;
-
-        for (int &n : adjList[node])
+        for (int i = 0; i < m; i++)
         {
-            // Runs for all the adjacent vertices for the current vertex.
-
-            if (!visited[n])
-            {
-                visited[n] = 1;
-                q.push({n, node});
-            }
-            // means the current adjacent node is visited, and if it's not the parent, then we know that we visited this node from another path, hence confirming the existence of a cycle.
-            else if (parent != n)
-            {
-                return true;
-            }
+            // looping from 0 to m-1 (inclusive).
+            int u, v;
+            cin >> u >> v;
+            this->adjList[u].emplace_back(v);
+            this->adjList[v].emplace_back(u);
         }
     }
 
-    return false;
-}
+    bool detectCycle_bfs(int start)
+    {
+
+        queue<pair<int, int>> q;
+        q.push({start, -1});
+
+        while (!q.empty())
+        {
+            int node = q.front().first, parent = q.front().second;
+            q.pop();
+            visited[node] = 1;
+
+            for (int &num : adjList[node])
+            {
+                // Runs for all the adjacent vertices for the current vertex.
+
+                if (!visited[num])
+                {
+                    visited[num] = 1;
+                    q.push({num, node});
+                }
+                // means the current adjacent node is visited, and if it's not the parent, then we know that we visited this node from another path, hence confirming the existence of a cycle.
+                else if (parent != num)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    bool helper(int node, int parent)
+    {
+
+        visited[node] = 1;
+
+        for (auto &num : adjList[node])
+        {
+
+            if (!visited[num])
+            {
+                bool tmp = helper(num, node);
+                if (tmp)
+                    return true;
+            }
+            else if (num != parent)
+                return true;
+        }
+        return false;
+    }
+
+    bool detectCycle_dfs(int node, int parent)
+    {
+        return helper(node, parent);
+    }
+
+    bool checkComponents_dfs(int n)
+    {
+
+        for (int i = 1; i <= n; i++)
+        {
+            // looping from 1 to n (inclusive).
+            if (!visited[i])
+            {
+                bool flag = detectCycle_dfs(i, -1);
+                if (flag)
+                    return flag;
+            }
+        }
+        return false;
+    }
+    void checkComponents()
+    {
+        bool ans = false;
+        for (int i = 1; i <= n; i++)
+        {
+
+            if (!visited[i])
+            {
+                if (detectCycle_bfs(i))
+                {
+                    ans = true;
+                    break;
+                }
+            }
+        }
+
+        (ans == true) ? cout << "cycle is present" << endl : cout << "cycle is not present" << endl;
+    }
+
+    vector<vector<int>> getAdjList()
+    {
+        return this->adjList;
+    }
+
+    vector<int> getVisited()
+    {
+        return this->visited;
+    }
+};
+
+class DirectedGraph
+{
+private:
+    vector<vector<int>> adjList;
+    vector<int> visited, pathVisited;
+    int n, m;
+
+public:
+    DirectedGraph(int n, int m)
+    {
+        this->n = n, this->m = m;
+        this->adjList = vector<vector<int>>(n + 1, vector<int>{});
+        this->visited = vector<int>(n + 1, 0);
+        this->pathVisited = vector<int>(n + 1, 0);
+    }
+
+    void constructAdjList()
+    {
+        for (int i = 0; i < m; i++)
+        {
+            // looping from 0 to m-1 (inclusive).
+            int u, v;
+            cin >> u >> v;
+            this->adjList[u].emplace_back(v);
+        }
+    }
+
+    bool cycleDetection_bfs()
+    {
+    }
+
+    bool helper(int start)
+    {
+
+        visited[start] = 1;
+        pathVisited[start] = 1;
+
+        for (int &num : adjList[start])
+        {
+
+            if (!visited[num])
+            {
+                bool flag = helper(num);
+                if (flag)
+                    return true;
+            }
+            else if (pathVisited[num])
+                return true;
+        }
+        pathVisited[start] = 0;
+        return false;
+    }
+
+    bool cycleDetection_dfs()
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            // looping from 1 to n (inclusive).
+            if (!visited[i])
+            {
+                if (helper(i))
+                    return true;
+            }
+        }
+        return false;
+    }
+};
 
 int main()
 {
-
-    vector<vector<int>> adjList;
 
     int n, m, start;
     cout << "Enter no of nodes:" << endl;
@@ -61,27 +210,13 @@ int main()
          << "Enter number of edges:" << endl;
     cin >> m;
 
-    adjList = constructAdjList(n, m);
-    vector<int> visited(n + 1, 0);
+    DirectedGraph *graph1 = new DirectedGraph(n, m);
+    graph1->constructAdjList();
+    if (graph1->cycleDetection_dfs())
+        cout << "cycle detected" << endl;
 
-    cout << "Enter the starting node:" << endl;
-    cin >> start;
-
-    bool ans = false;
-    for (int i = 1; i <= n; i++)
-    {
-
-        if (!visited[i])
-        {
-            if (detectCycle(adjList, visited, i))
-            {
-                ans = true;
-                break;
-            }
-        }
-    }
-
-    (ans == true) ? cout << "cycle is present" << endl : cout << "cycle is not present" << endl;
+    else
+        cout << "Cycle not detected" << endl;
 
     return 0;
 }
