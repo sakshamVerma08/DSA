11b9f695cc8e04cfc724234985b54097caa1add6
Saksham Verma <sakshamverma1000@gmail.com>
2026-06-15 20:36:18 +0530

    did one leetcode potd

---
+++ Leetcode POTD/15_06_2026.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ 
+class Solution {
+public:
+Solution(){
+
+}
+    ListNode* deleteMiddle(ListNode* head) {
+        ListNode* temp = head;
+        int n = 0;
+        while(temp){
+            n++;
+            temp=temp->next;
+        }
+        
+        if(n==1)
+            return nullptr;
+            
+        temp=head;
+
+        int mid_idx = n/2;
+
+        int i = 0;
+        while(temp && i < mid_idx-1){
+            i++;
+            temp=temp->next;
+        }
+
+        
+
+        ListNode* forward = temp->next->next;
+        ListNode* del = temp->next;
+        del->next=nullptr;
+        temp->next=forward;
+        delete del;
+        return head;
+
+    }
+};
+
+int main(){
+
+
+}
